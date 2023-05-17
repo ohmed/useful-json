b5b52010fcaa28ca49890229e138bfcf5f87ae8c
Oduor Mike <87503468+AnonyMyke@users.noreply.github.com>
2023-05-17 21:50:00 +0300

    yes

--- US States.py
+++ US States.py
@@ -1,1 +1,104 @@
 import json
+# Define the state information
+state_data = {
+    'name': '',
+    'id': '',
+    'initials': '',
+    'headquarter': '',
+    'date_created': '1978-07-01',
+    'date_updated': '2018-07-02',
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
